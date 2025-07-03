8ebbf802b62886555a51ee43579ac16d15fe3f60
zzj-ML <2874037623@qq.com>
2025-07-04 06:10:45 +0800

    Add files via upload

---
+++ XML/MLP.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy import stats
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from joblib import dump,load
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.model_selection import GridSearchCV, ShuffleSplit
+
+
+df = pd.read_csv('...')
+
+X = df.drop(columns=['RDFT'])
+Y = df['RDFT']
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=10)
+print("X_train's shape is", X_train.shape,"; y_train's shape is", y_train.shape)
+print("X_test's shape is", X_test.shape,"; y_test's shape is",y_test.shape)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train_stand = scaler.transform(X_train)
+X_test_stand = scaler.transform(X_test)
+
+
+
+param_grid = {"hidden_layer_sizes":[(5,),(5,2)]
+     ,"activation":["relu","logistic","tanh"]
+     ,"learning_rate_init":[0.01,0.1]
+     ,"max_iter":[100,200,500]
+    }
+
+mlpmodel = MLPRegressor()
+
+grid_search = GridSearchCV(estimator=mlpmodel, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
+grid_search.fit(X_train, y_train)
+
+best_model = grid_search.best_estimator_
+print("Best parameters found:", grid_search.best_params_)
+
+mlpmodel.fit(X_train_stand, y_train)
+y_train_hat = mlpmodel.predict(X_train_stand)
+y_test_hat = mlpmodel.predict(X_test_stand)
+
+
+fontsize = 22
+plt.figure(figsize=(8, 6))
+
+a = plt.scatter(y_train, y_train_hat, s=150, c=[(104/255, 136/255, 245/255)], label='Train')
+
+b = plt.scatter(y_test, y_test_hat, s=150, c=[(215/255, 112/255, 113/255)], label='Test')
+
+plt.plot([1.5, 6.3], [1.5, 6.3], color='k', linestyle='--')
+
+plt.xlabel('Experimental value of RFDT', fontsize=fontsize)
+plt.ylabel('ML predicted value of RFDT', fontsize=fontsize)
+
+r2 = r2_score(y_test, y_test_hat)
+mae = mean_absolute_error(y_test, y_test_hat)
+rmse = np.sqrt(mean_squared_error(y_test, y_test_hat))
+
+plt.text(2.37, 5.75, f' MAE={mae:.2f}, RMSE={rmse:.2f}', fontsize=24)
+
+plt.xlim([1.5, 6.3])
+plt.ylim([1.5, 6.3])
+
+plt.legend(fontsize=18, handletextpad=0.1, borderpad=0.1)
+plt.tick_params(axis='both', which='major', labelsize=fontsize)
+
+plt.tight_layout()
+
+# plt.savefig('mlp_pybel.png', dpi = 1000)
+plt.show()
+
+
+
+
